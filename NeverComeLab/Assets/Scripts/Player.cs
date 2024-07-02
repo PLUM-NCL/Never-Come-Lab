32a52ba0e6177c438f954d5de3ce0df0b866e860
HYOJEONG0213 <88478785+HYOJEONG0213@users.noreply.github.com>
2024-07-02 18:43:43 +0900

    양효정 - 플레이어 이동, 애니메이션

---
+++ NeverComeLab/Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+    Animator anim;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        inputVec.x = Input.GetAxis("Horizontal");
+        inputVec.y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    private void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude); //애니메이션 Float값 수정, 벡터의 순수한 크기 값
+
+        anim.ResetTrigger("Forward");
+        anim.ResetTrigger("Back");
+        anim.ResetTrigger("Right");
+
+        if (inputVec.y > 0)
+        {
+            anim.SetTrigger("Forward");
+        }
+        else if (inputVec.y < 0)
+        {
+            anim.SetTrigger("Back");
+        }
+
+        else if (inputVec.x > 0)
+        {
+            anim.SetTrigger("Right");
+            spriter.flipX = false;
+        }
+        else if (inputVec.x < 0)
+        {
+            anim.SetTrigger("Right");
+            spriter.flipX = true;
+        }
+    }
+}
