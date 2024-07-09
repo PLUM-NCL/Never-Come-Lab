df644352e2853887bb2618b29c0b93c742c01d8f
Jisub Shim <jisub5322@naver.com>
2024-07-09 22:12:34 +0900

    feat: 몬스터 상태별 이동 구현
    Patrol, Chase, Return 구현

--- NeverComeLab/Assets/Scripts/Monster.cs
+++ NeverComeLab/Assets/Scripts/Monster.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Monster : MonoBehaviour
 {
+    private enum State { Patrol, Chase, Return }
+    private State currentState = State.Patrol;
+
+    private Vector3 initialPosition;
     private Vector3 target;
     NavMeshAgent agent;
+    public TextMeshPro mark;
+
 
     [SerializeField]
     private MonsterData enemyData;
@@ -17,24 +24,23 @@ public class Monster : MonoBehaviour
     [SerializeField]
     Transform pos;
 
-    private float monsterHp; // 체력
-    private float monsterDamage; // 공격력
-    private float monsterSpeed; // 이속
-    private float monsterAttackSpeed; // 공속 (낮을 수록 빠름) 딜레이 시간
-    private int monsterType; // 몬스터 유형
+    private float monsterHp;
+    private float monsterDamage;
+    private float monsterSpeed;
+    private float monsterAttackSpeed;
+    private int monsterType;
 
     private bool isHit = false;
     private bool isShooting = false;
-    private bool isDie = false; // 죽었는지 안죽었는지
+    private bool isDie = false;
     private bool isPlayerDetected = false;
 
     private Animator monsterAnimator;
     private AudioSource monsterAudio;
 
-    private float distanceToPlayer; // 플레이어와의 거리
-    private bool isAttack = false; // 공격 구분
-    private float attackSpeed = 1f; // 공속 (공격 간격)
-    private float stopChasingDistance = 5f; // 추적을 멈출 거리
+    private float distanceToPlayer;
+    private float attackSpeed = 1f;
+    private float stopChasingDistance = 5f;
 
     public Transform player;
     public float projectileSpeed = 5f;
@@ -54,8 +60,10 @@ public class Monster : MonoBehaviour
         agent.updateRotation = false;
         agent.updateUpAxis = false;
     }
+
     private void Start()
     {
+        initialPosition = transform.position;
         monsterHp = enemyData.monsterHp;
         monsterDamage = enemyData.monsterDamage;
         monsterAttackSpeed = enemyData.monsterAttackSpeed;
@@ -65,16 +73,55 @@ public class Monster : MonoBehaviour
         monsterAnimator = GetComponent<Animator>();
         monsterAudio = GetComponent<AudioSource>();
         rigid = GetComponent<Rigidbody2D>();
-    }
-    void SetAgentPosition()
-    {
-        agent.SetDestination(player.position);
+
+        StartCoroutine(PatrolRoutine());
     }
 
     private void Update()
     {
-        
+        distanceToPlayer = Vector2.Distance(transform.position, player.position);
 
+        switch (currentState)
+        {
+            case State.Patrol:
+                if (isPlayerDetected && distanceToPlayer <= stopChasingDistance)
+                {
+                    currentState = State.Chase;
+                    StopCoroutine(PatrolRoutine());
+                    mark.text = "!";
+                }
+                break;
+            case State.Chase:
+               Chase();
+                if (distanceToPlayer > stopChasingDistance)
+                {
+                    SetPlayerDetected(false);
+                    currentState = State.Return;
+                    mark.text = "?";
+                    agent.velocity = Vector2.zero;
+                }
+                break;
+            case State.Return:
+                
+                Invoke("Return", 3);
+                if (Vector2.Distance(transform.position, initialPosition) < 0.1f)
+                {
+                    mark.text = "";
+                    currentState = State.Patrol;
+                    StartCoroutine(PatrolRoutine());
+                }
+                break;
+        }
+    }
+
+    private void Patrol()
+    {
+        // 여기에 일정 간격으로 움직이는 로직을 구현
+        monsterAnimator.SetBool("isMove", true);
+    }
+
+    private void Chase()
+    {
         if (!isDie && isPlayerDetected)
         {
             if (!isShooting)
@@ -83,31 +130,34 @@ public class Monster : MonoBehaviour
             }
             else
             {
-                // 플레이어와 몬스터 사이의 거리 계산
-                distanceToPlayer = Vector2.Distance(transform.position, player.position);
-                Debug.Log(distanceToPlayer + "m");
-
-                if (distanceToPlayer > stopChasingDistance)
-                {
-                    // 플레이어와의 거리가 stopChasingDistance보다 크면 추적 중지
-                    SetPlayerDetected(false);
-                    agent.isStopped = true; // NavMeshAgent 멈추기
-                    agent.ResetPath(); // 현재 경로 초기화
-                    monsterAnimator.SetBool("isMove", false);
-                }
-                else
-                {
-                    // 플레이어 방향으로 몬스터 이동
-                    //Vector2 direction = (player.position - transform.position).normalized;
-                    //rigid.velocity = direction * monsterSpeed;
-                    SetAgentPosition();
-                    agent.isStopped = false;
-                    monsterAnimator.SetBool("isMove", true);
-                }
+                agent.SetDestination(player.position);
+                agent.isStopped = false;
+                monsterAnimator.SetBool("isMove", true);
             }
         }
     }
-    
+
+    private void Return()
+    {
+        
+        agent.SetDestination(initialPosition);
+        agent.isStopped = false;
+        monsterAnimator.SetBool("isMove", true);
+    }
+
+    IEnumerator PatrolRoutine()
+    {
+        while (currentState == State.Patrol)
+        {
+            // 왼쪽으로 이동
+            agent.SetDestination(new Vector3(initialPosition.x - 2, initialPosition.y, initialPosition.z));
+            yield return new WaitForSeconds(2f); // 2초 동안 이동
+
+            // 오른쪽으로 이동
+            agent.SetDestination(new Vector3(initialPosition.x + 2, initialPosition.y, initialPosition.z));
+            yield return new WaitForSeconds(2f); // 2초 동안 이동
+        }
+    }
 
     IEnumerator Shoot()
     {
