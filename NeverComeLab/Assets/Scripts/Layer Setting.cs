b54ca1c6744930e6dfaf5c1f27f2b83bebc86890
HYOJEONG0213 <fredflem@naver.com>
2024-08-04 12:44:32 +0900

    feat: 양효정 - 나무 뒤에 숨기 구현 #25
    아직 몬스터 추적 금지 기능 추가 안함

---
+++ NeverComeLab/Assets/Scripts/Layer Setting.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerSetting : MonoBehaviour
+{
+    SpriteRenderer sprite;
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.sortingOrder = Mathf.RoundToInt(transform.position.y) * -1;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            sprite.color = new Color(1, 1, 1, 0.5f);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            sprite.color = new Color(1, 1, 1, 1f);
+        }
+    }
+}
